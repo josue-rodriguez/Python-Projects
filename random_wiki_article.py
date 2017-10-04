df30ed9166178c7c0a3c41e57cbe00aafe64ed42
Josue Rodriguez <josue.rodriguez594@gmail.com>
2017-10-04 12:29:34 -0700

    Title Change + URL
    Changed title of script.
    
    Added url variable in order to open proper article from json data.

--- random_wiki_article.py
+++ random_wiki_article.py
@@ -11,19 +11,27 @@ while True:
         count = 0
 
         article_title = data['query']['random'][count]['title']
-        text = 'Would you like to read about {}?\nY/N?'.format(article_title)
+
+        text = 'Would you like to read about {}?\nY/N?\n'.format(article_title)
         answer = input(text)
 
         if answer[0].lower() == 'y':
+            url_id = data['query']['random'][count]['id']
+            url = 'http://en.wikipedia.org/wiki?curid={}'.format(url_id)
+
             webbrowser.open(url, new=2, autoraise=True)
 
         elif answer[0].lower() == 'n':
             while answer[0].lower() == 'n':
                 count += 1
                 article_title = data['query']['random'][count]['title']
-                text = 'Would you like to read about {}?\nY/N?'.format(article_title)
+                text = 'Would you like to read about {}?\nY/N?\n'.format(article_title)
                 answer = input(text)
-            """ keep doing stuff """
+
+            url_id = data['query']['random'][count]['id']
+            url = 'http://en.wikipedia.org/wiki?curid={}'.format(url_id)
+
+            webbrowser.open(url, new=2, autoraise=True)
 
         else:
             print('The answer you entered is not valid. Please try again\n')
